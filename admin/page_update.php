e0d3740ed67f31dea4c392eca46005127e65db4a
Anthony Rossbach <me@anthonys.space>
2018-12-28 02:18:04 -0800

    Update system now uses GIT

--- admin/page_update.php
+++ admin/page_update.php
@@ -4,18 +4,8 @@ logincheck();
 echo '<h1>Update SS</h1>';
 echo 'Below we will list any important system updates that should be installed. We are checking for updates now.';
 
-if (!isset($system_data["core_branch"])){
-  $system_data["core_branch"]="stable";
-  $system_data_update=true;
-}
-
-if ($system_data["core_branch"]=="master"){
-  $system_data["core_branch"]="stable";
-  $system_data_update=true;
-}
-
 //--Get current version from GITHUB
-$data=json_decode(utf8_encode(get_contents('https://downloads.codesimplescript.com/version_'.$system_data["core_branch"].'.json')), true); //Fetch Config Data
+$data=json_decode(utf8_encode(get_contents('https://raw.githubusercontent.com/CodeSimpleScript/core/master/version.json')), true); //Fetch Config Data
 
 if (strval($system_data["core_version"])!=strval($data["version"])){
   echo '<div onclick="modl(\''.$settings["admin_url"].'?page=update_install&ui_nostyle=true\');" style="background:rgb(231,76,60);color:#ffffff;padding:5px;margin-top:5px;margin-bottom:5px;border-radius:2px;">An update is avalable for SimpleScript ('.$data["version"].'), click to update. You are running version ('.$system_data["core_version"].').</div><div><h3>Update Notes</h3>'.$data["message"].'<BR><BR>For more information visit <a href="https://www.codesimplescript.com/changelog" target="_new">codesimplescript.com/changelog</a></div>';
--- admin/page_update_install.php
+++ admin/page_update_install.php
@@ -6,13 +6,11 @@ if (!copy("admin/update.php", "ss-run.php")){
   echo "Install run failed";
 }else{
   //--Patch Data File
-  if ($system_data["core_branch"]=="master"){ $system_data["core_branch"]="stable"; }
-
-  $data=json_decode(utf8_encode(get_contents('https://downloads.codesimplescript.com/version_'.$system_data["core_branch"].'.json')), true); //Fetch Config Data
+  $data=json_decode(utf8_encode(get_contents('https://raw.githubusercontent.com/CodeSimpleScript/core/master/version.json')), true); //Fetch Config Data
   if (isset($system_data["version"])){ unset($system_data["version"]); }
   $system_data["core_version"]=$data["version"];
   $system_data_update=true;
-  echo "Just wait, installing...<script>window.setTimeout(function(){ modl(\"ss-run.php?zip=".urlencode("https://downloads.codesimplescript.com/".$system_data["core_branch"].".zip")."&admin_url=".urlencode($settings["admin_url"])."&page=update_done&ui_nostyle=true\"); }, 500);</script>";
+  echo "Just wait, installing...<script>window.setTimeout(function(){ modl(\"ss-run.php?zip=".urlencode("https://github.com/CodeSimpleScript/core/archive/master.zip")."&admin_url=".urlencode($settings["admin_url"])."&page=update_done&ui_nostyle=true\"); }, 500);</script>";
 }
 
 ?>
